d3dfaf4f169d3b3d269a18b992922b223601102f
Fl0ydR0se <40611172+Fl0ydR0se@users.noreply.github.com>
2018-07-06 20:27:13 +0300

    Update search.appartment.js

--- pageobjects/Booking/search.appartment.js
+++ pageobjects/Booking/search.appartment.js
@@ -1,8 +1,10 @@
 import Page from '../page';
 
 class SearchAppartment extends Page {
-	get NameApp() {return browser.element('#ss');}
-	
+	get Nameapp() {return browser.element('#ss');}
+	 verifyOnPage () {return browser.element('#logo_no_globe_new_logo');}
+	get Searchinput () {return browser.element('.c-autocomplete__input .sb-searchbox__input .sb-destination__input #')} 
+
 	open(){
 
 		super.open('/')
