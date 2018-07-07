4a0b71df827661ae420a8daa778ab95118806619
Oleg <againstlies2@Gmail.com>
2018-07-07 16:29:44 +0300

    V3.1

--- pageobjects/Booking/search.appartment.js
+++ pageobjects/Booking/search.appartment.js
@@ -1,14 +1,13 @@
 import Page from '../page';
 
 class SearchAppartment extends Page {
-	get nameApp() {return browser.element('#ss');}
-	 verifyOnPage () {return browser.element('#logo_no_globe_new_logo');}
-	get Searchinput () {return browser.element('.c-autocomplete__input .sb-searchbox__input .sb-destination__input #')} 
+	get nameApp    () {return browser.element('#ss');}
+	verifyOnPage   () {return browser.element('#logo_no_globe_new_logo');}
+	clickHotelName () {return browser.element('span.sr-hotel__name*=Hotel Pennsylvania').element("..").click()}
+	clickBuyButton () {return browser.element ('#right #top-book .b-button_primary').click()}
 
-	open(){
 
-		super.open('/')
-	}
+open() {super.open('/')	}
 }
 
 export default new SearchAppartment()
--- specs/Booking.SearchAppartment.spec.js
+++ specs/Booking.SearchAppartment.spec.js
@@ -1,35 +1,37 @@
 import SearchAppartment from '../pageobjects/Booking/search.appartment'
 
 require('chai').should();
-
 var assert = require('chai').assert
 
 describe('Search the appartment', () => {
-	
-   it('Open site', () => {
+    it('Open site', () => {
         SearchAppartment.open()
         SearchAppartment.verifyOnPage()
     })
+
     it('Search "Hotel Pennsylvania"', () =>{
 
-       browser.element('.c-autocomplete__input .sb-searchbox__input .sb-destination__input').should.exist
-       SearchAppartment.nameApp.setValue('Hotel Pennsylvania')
-       browser.click('.sb-searchbox__button')
-       browser.element('#hotel_1803065').should.exist
-       console.log(browser.getCurrentTabId())
-       browser.getTitle()
-       browser.element('span.sr-hotel__name*=Hotel Pennsylvania').element("..").click();
+      // browser.element('.c-autocomplete__input .sb-searchbox__input .sb-destination__input').should.exist
+      SearchAppartment.nameApp.setValue('Hotel Pennsylvania')
+      browser.click('.sb-searchbox__button')
+      // browser.element('#hotel_1803065').should.exist
+      console.log(browser.getCurrentTabId())
+      browser.getTitle()
+      SearchAppartment.clickHotelName()
+      console.log(browser.windowHandle());
       browser.getTitle()
       browser.pause(15000)
-       console.log(browser.getCurrentTabId())
+      console.log(browser.getCurrentTabId())
 
-       var t = browser.getUrl();
-       browser.newWindow(t);
-       
-       browser.click('#right #top-book .b-button_primary')
-       browser.getTitle()
-       console.log(browser.getCurrentTabId())
-       browser.pause(15000)
+      var t = browser.getUrl();
+      browser.newWindow(t);
+
+       //browser.click('#right #top-book .b-button_primary')
+      SearchAppartment.clickBuyButton()
+      browser.getTitle()
+      console.log(browser.getCurrentTabId())
+      console.log(browser.windowHandle());
+      browser.pause(15000)
        
  })
     
