2b869a094e185cb2b2b29c11c2e579a38485b6a0
Oleg <againstlies2@Gmail.com>
2018-07-07 15:45:42 +0300

    v3

--- pageobjects/Booking/search.appartment.js
+++ pageobjects/Booking/search.appartment.js
@@ -1,7 +1,7 @@
 import Page from '../page';
 
 class SearchAppartment extends Page {
-	get Nameapp() {return browser.element('#ss');}
+	get nameApp() {return browser.element('#ss');}
 	 verifyOnPage () {return browser.element('#logo_no_globe_new_logo');}
 	get Searchinput () {return browser.element('.c-autocomplete__input .sb-searchbox__input .sb-destination__input #')} 
 
--- specs/Booking.SearchAppartment.spec.js
+++ specs/Booking.SearchAppartment.spec.js
@@ -13,7 +13,7 @@ describe('Search the appartment', () => {
     it('Search "Hotel Pennsylvania"', () =>{
 
        browser.element('.c-autocomplete__input .sb-searchbox__input .sb-destination__input').should.exist
-       SearchAppartment.NameApp.setValue('Hotel Pennsylvania')
+       SearchAppartment.nameApp.setValue('Hotel Pennsylvania')
        browser.click('.sb-searchbox__button')
        browser.element('#hotel_1803065').should.exist
        console.log(browser.getCurrentTabId())
@@ -22,9 +22,10 @@ describe('Search the appartment', () => {
       browser.getTitle()
       browser.pause(15000)
        console.log(browser.getCurrentTabId())
+
        var t = browser.getUrl();
-       
        browser.newWindow(t);
+       
        browser.click('#right #top-book .b-button_primary')
        browser.getTitle()
        console.log(browser.getCurrentTabId())
