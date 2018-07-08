e9426a8265f81eb607bd75668a06f2cd025482f9
Oleg <againstlies2@Gmail.com>
2018-07-08 19:01:18 +0300

    eslint fix

--- pageobjects/Booking/search.appartment.js
+++ pageobjects/Booking/search.appartment.js
@@ -1,20 +1,20 @@
-import Page from '../page';
+import Page from "../page";
 
-require('chai').should();
+require("chai").should();
 
 class SearchAppartment extends Page {
-	get searchInput    () {return browser.element('#ss');}
-	get searchButton () {return browser.element ('.sb-searchbox__button')}
+	get searchInput    () {return browser.element("#ss");}
+	get searchButton () {return browser.element (".sb-searchbox__button");}
 	
 
 	verifyOnPage() {
-		let logoHref = browser.element('#logo_no_globe_new_logo').getAttribute('src');
-		logoHref.should.include('booking_logo_retina/22615963add19ac6b6d715a97c8d477e8b95b7ea.png');
+		let logoHref = browser.element("#logo_no_globe_new_logo").getAttribute("src");
+		logoHref.should.include("booking_logo_retina/22615963add19ac6b6d715a97c8d477e8b95b7ea.png");
 	}
 
 	search(hotelName) {
 		this.searchInput.setValue(hotelName);
-      	this.searchButton.click();
+		this.searchButton.click();
 	}
 
 	checkResult(elementId) {
@@ -22,27 +22,27 @@ class SearchAppartment extends Page {
 	}
 
 	open() {
-		super.open('/')	
+		super.open("/");	
 	}
 
 	selectHotel(elementId) {
 		
-		let calendarDiv = browser.element('.sb-date-field__wrapper .c2-wrapper');
-		if(!calendarDiv.getAttribute('class').includes('c2-wrapper-s-hidden')) {
-			calendarDiv.element('.sb-searchbox__input').click()
+		let calendarDiv = browser.element(".sb-date-field__wrapper .c2-wrapper");
+		if(!calendarDiv.getAttribute("class").includes("c2-wrapper-s-hidden")) {
+			calendarDiv.element(".sb-searchbox__input").click();
 		}
-		browser.element(elementId).element('.sr_item_photo_link').click()
+		browser.element(elementId).element(".sr_item_photo_link").click();
 	}
 
 	checkNewTab() {
-     let handles = browser.windowHandles().value
-     handles.length.should.equal(2)
-     handles[0].should.equal(browser.getCurrentTabId())
+		let handles = browser.windowHandles().value;
+		handles.length.should.equal(2);
+		handles[0].should.equal(browser.getCurrentTabId());
 	}
 
 	pausePage(milliseconds) {
-	    browser.pause(milliseconds)
+		browser.pause(milliseconds);
 	}
 }
 
-export default new SearchAppartment()
+export default new SearchAppartment();
