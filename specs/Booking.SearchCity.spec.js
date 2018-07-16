c6f200c0aa0274ab38e6fb989865b489f58f586e
Oleg <againstlies2@Gmail.com>
2018-07-17 02:16:27 +0300

    getValue  getText

--- specs/Booking.SearchCity.spec.js
+++ specs/Booking.SearchCity.spec.js
@@ -1,5 +1,6 @@
 import SearchCity from "../pageobjects/Booking/search.city";
 
+/* global browser */
 
 require("chai").should();
 
@@ -15,14 +16,19 @@ require("chai").should();
 		it(`Search the city ${city}`, () =>{
 		
 			SearchCity.search(city);
+			let t = $("#ss");
+			let n = t.getValue();
+			let b = t.getText();
+			console.log(n);
+			console.log(b);		
 
 		});
 
-		it("Select October 10-11", () =>{
+		// it("Select October 10-11", () =>{
 
-			SearchCity.selectDate("Oct 10 2018 UTC");
+		// 	SearchCity.selectDate("Oct 10 2018 UTC");
 
-		});
+		// });
 
 	})
 );
