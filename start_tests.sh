6cf83b66bd8e954d776a13949ad332589eca9286
captify-lazariev <oleg.lazariev@captify.co.uk>
2019-07-02 00:12:13 +0300

    start stop mthrfkr

--- start_tests.sh
+++ start_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 docker-compose run e2e-test
 
+TEST_EXIT_CODE=$?
+
+echo "Exit code is $TEST_EXIT_CODE"
+if [[ $TEST_EXIT_CODE -eq 0 ]]; then
+    echo "Tests e2e stil work!"
+else
+    echo " fix me please!"
+fi
+
 ./clean.sh
